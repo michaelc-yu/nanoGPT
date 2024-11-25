f657afc80ad1fd597145865273d5f7abed552c47
michaelc-yu <woolythesheep@gmail.com>
2024-11-25 15:24:11 -0800

    update

--- sample.py
+++ sample.py
@@ -11,9 +11,23 @@ from model import GPTConfig, GPT
 # -----------------------------------------------------------------------------
 init_from = 'resume' # either 'resume' (from an out_dir) or a gpt2 variant (e.g. 'gpt2-xl')
 out_dir = 'out' # ignored if init_from is not 'resume'
-start = "\n" # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
+# start = "\n" # or "<|endoftext|>" or etc. Can also specify a file, use as: "FILE:prompt.txt"
+start = '''<input>
+0 0 8 1 6 7 0 2 0 
+5 0 0 2 3 0 0 0 0 
+7 6 0 0 5 4 8 0 1 
+8 7 0 0 4 0 0 0 0 
+0 2 0 0 0 0 0 0 0 
+0 0 4 0 0 3 0 9 0 
+0 0 0 0 0 0 3 7 0 
+0 4 0 0 0 0 0 8 0 
+3 1 0 8 0 6 9 0 4 
+</input>
+
+'''
+
 num_samples = 10 # number of samples to draw
-max_new_tokens = 500 # number of tokens generated in each sample
+max_new_tokens = 50000 # number of tokens generated in each sample
 temperature = 0.8 # 1.0 = no change, < 1.0 = less random, > 1.0 = more random, in predictions
 top_k = 200 # retain only the top_k most likely tokens, clamp others to have 0 probability
 seed = 1337
